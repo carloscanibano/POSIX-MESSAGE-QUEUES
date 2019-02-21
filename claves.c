85d408c58e9ac73a2bd2377247d8e0a058af41db
carlosgarciacanibano <garciacanibanocarlos@gmail.com>
2019-02-21 17:26:00 +0100

    init function

--- claves.c
+++ claves.c
@@ -10,7 +10,49 @@
 #include "claves.h"
 
 int init() {
+	mqd_t q_server;
+	mqd_t q_client;
+	struct mq_attr attrib;
+	struct request req;
+	struct answer ans;
+	char *id;
+	char *id_server = "Q_SERVER";
 
+	sprintf(id, "client_%d", getpid());
+	if ((q_client = mq_open(id, O_CREAT | O_RDWR, 0777, &attrib)) == -1) {
+		printf("Error al abrir la cola de cliente.\n");
+		exit(-1);
+	}
+
+	if ((q_server = mq_open(id_server, O_WRONLY)) == -1) {
+		printf("Error al abrir la cola del servidor.\n");
+	}
+
+	//Setting up request
+	req.op = 0;
+	strcpy(req.q_name, id);
+
+	if ((mq_send(q_server, (const char *) &req, sizeof(req), 0)) == -1) {
+		printf("La petición no ha llegado al servidor.\n");
+	}
+
+	if ((mq_receive(q_client, (char *) &ans, sizeof(ans), 0)) == -1) {
+		printf("No se ha podido recibir la respuesta del servidor.\n");
+	}
+
+	if ((mq_close(q_server)) == -1) {
+		printf("Error al cerrar la cola del servidor.\n");
+	}
+
+	if ((mq_close(q_client)) == -1) {
+		printf("Error al cerrar la cola del cliente.\n");
+	}
+
+	if ((mq_unlink(id)) == -1) {
+		printf("Error al eliminar la cola del cliente.\n");
+	}
+
+	return ans.answer_code;
 }
 
 //Insert new element into the server queue
