9374aa8b5f8d19c3fccdad5b6022f77ddaaed6de
carlosgarciacanibano <garciacanibanocarlos@gmail.com>
2019-02-22 14:20:48 +0100

    added functions
    *NO TESTED*

--- linked_list.c
+++ linked_list.c
@@ -4,15 +4,22 @@
 #include <stddef.h>
 #include <string.h>
 
-void insert(Triplet_list *l, struct triplet *t){
+int insert(Triplet_list l, struct triplet *t){
 	Triplet_list p1, p2;
-	p1 = *l;
+	p1 = l;
+	int code = -1;
+
+	if (search(l, t->key) == NULL) {
+		return code;
+	}
+
 	if (p1 == NULL) {
 		p1 = (struct triplet *) malloc(sizeof(struct triplet));
 		strcpy(p1->key, t->key);
 		strcpy(p1->first_value,t->first_value);
 		p1->second_value = t->second_value;
 		p1->link = NULL;
+		code = 0;
 	} else {
 		while (p1->link != NULL) {
 			p1 = p1->link;
@@ -22,7 +29,9 @@ void insert(Triplet_list *l, struct triplet *t){
 		strcpy(p2->first_value, t->first_value);
 		p2->second_value = t->second_value;
 		p1->link = p2;
+		code = 0;
 	}
+	return code;
 }
 
 void show(Triplet_list l) {
@@ -43,4 +52,60 @@ void erase(Triplet_list *l) {
 		}
 		*l = NULL;
 	}
+}
+
+int elements(Triplet_list l) {
+	Triplet_list cursor = l;
+	int c = 0;
+	while (cursor != NULL) {
+		c++;
+		cursor = cursor->link;
+	}
+	return c;
+}
+
+struct triplet* search(Triplet_list l, char *key) {
+	Triplet_list cursor = l;
+	while (cursor != NULL) {
+		if (strcmp(cursor->key, key) == 0) {
+			return cursor;
+		}
+		cursor = cursor->link;
+	}
+	return NULL;
+}
+
+int modify(Triplet_list l, char *key, char *value1, float value2) {
+	int code = -1;
+	Triplet_list cursor = l;
+	if (search(l, key) == NULL) {
+		return code;
+	} else {
+		while (cursor != NULL) {
+			if (strcmp(cursor->key, key) == 0) {
+				strcmp(cursor->first_value, value1);
+				cursor->second_value = value2;
+				return 0;
+			}
+			cursor = cursor->link;
+		}	
+	}
+	return code;
+}
+
+int delete(Triplet_list l, char *key) {
+	int code = -1;
+	Triplet_list cursor = l;
+	if (search(l, key) == NULL) {
+		return code;
+	} else {
+		while (cursor != NULL) {
+			if (strcmp(cursor->link->key, key) == 0) {
+				cursor->link = cursor->link->link;
+				free(cursor->link);
+				return 0;
+			}
+		}
+	}
+	return code;
 }
